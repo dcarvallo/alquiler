45c57ee2df4ffa2209dede55ac2df9a320361436
daniel.carvallo <decarvallo@organojudicial.gob.bo>
2022-02-19 22:27:20 -0400

    Commit inicial

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('inicio')
+})
+
+app.listen(port, () => {
+  console.log(`app en puerto:  ${port}`)
+})
